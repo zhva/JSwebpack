548cd4d4cb5cf5c22667538a2370202d79ec0cdc
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2022-11-18 15:11:25 +0100

    HTML Plugin

--- 04-webpack/webpack.config.js
+++ 04-webpack/webpack.config.js
@@ -1,10 +1,11 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
 
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.js",
+    filename: "[name].[contenthash].js",
   },
   module: {
     rules: [
@@ -29,5 +30,11 @@ module.exports = {
   devServer: {
     static: './dist',
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "src/index.html",
+    }),
+  ],
+
 };
 
