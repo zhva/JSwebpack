959e0c8d6f01ea89c7a1360c723c63bbc609a30f
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2022-11-18 14:55:15 +0100

    Dev Server

--- 04-webpack/src/index.js
+++ 04-webpack/src/index.js
@@ -5,7 +5,7 @@ function component() {
   const element = document.createElement("div");
 
   // Uses the Lodash library to join the array
-  element.innerHTML = join(["Hello", "webpack"], " ");
+  element.innerHTML = join(["Hello!!!", "webpack"], " ");
 
   return element;
 }
--- 04-webpack/webpack.config.js
+++ 04-webpack/webpack.config.js
@@ -21,5 +21,8 @@ module.exports = {
       },
     ],
   },
+  devServer: {
+    static: './dist',
+  },
 };
 
