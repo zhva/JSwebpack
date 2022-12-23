bf4084e7c5268749d8023632759a6c860125a2c2
Viktoriia <vzhuravel.mmt-b2021@fh-salzburg.ac.at>
2022-12-23 12:30:06 +0100

    changes app configuration in  webpack.config

--- 04-webpack/webpack.config.js
+++ 04-webpack/webpack.config.js
@@ -1,5 +1,7 @@
-const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+const devMode = process.env.NODE_ENV !== "production"; // See note below
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 module.exports = {
   entry: "./src/index.js",
@@ -7,34 +9,36 @@ module.exports = {
     path: path.resolve(__dirname, "dist"),
     filename: "[name].[contenthash].js",
   },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist"),
+    },
+    compress: true,
+    port: 9000,
+  },
   module: {
     rules: [
       {
         test: /\.s[ac]ss$/i,
         use: [
-            // Creates `style` nodes from JS strings
-            "style-loader",
-            // Translates CSS into CommonJS
-            "css-loader",
-            // Compiles Sass to CSS
-            "sass-loader",
+          MiniCssExtractPlugin.loader, // Replace the style loader with the new plugin
+          "css-loader",
+          "sass-loader",
         ],
       },
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: "asset/resource",
       },
-
     ],
-  },
-  devServer: {
-    static: './dist',
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: "src/index.html",
     }),
+    // Init the new plugin
+    new MiniCssExtractPlugin({
+      filename: devMode ? "[name].css" : "[name].[contenthash].css",
+    }),
   ],
-
 };
-
